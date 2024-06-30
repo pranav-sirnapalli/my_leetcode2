00633787e5340d089449f0cf95c20bcad9d6ecb2
pranav sirnapalli <68234635+pranav-sirnapalli@users.noreply.github.com>
2024-06-30 16:05:02 +0530

    Time: 48 ms (96.63%), Space: 16.6 MB (55.18%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,25 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        write = 0
+        read = 0
+
+        while read < len(chars):
+            char = chars[read]
+            count = 0
+        
+            # Count occur of char
+            while read<len(chars) and chars[read] == char:
+                read += 1
+                count += 1
+
+            # write that char to write pos
+            chars[write] = char
+            write += 1
+
+            # If the char count is more than 1, write the count as well
+            if count > 1:
+                for digit in str(count):
+                    chars[write] = digit
+                    write += 1
+        
+        return write
